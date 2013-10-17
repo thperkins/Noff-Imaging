a857bb2754c012f45461eab3ca73ba4793ea162e
thperkins <thperkins@users.noreply.github.com>
2013-10-17 10:21:48 +0200

    Added comments

--- img-to-matrix.php
+++ img-to-matrix.php
@@ -1,26 +1,26 @@
 <?php
 
 // open an image
-$im = imagecreatefrompng('nexen.png');
+$img = imagecreatefromjpeg('image.jpg'); // imagecreatefromjpeg/png/
 
 // get a color
 $start_x = 40;
 $start_y = 50;
-$color_index = imagecolorat($im, $start_x, $start_y);
+$color_index = imagecolorat($img, $start_x, $start_y);
 
 // make it human readable
-$color_tran = imagecolorsforindex($im, $color_index);
+$color_tran = imagecolorsforindex($img, $color_index);
 
 // what is it ?
 print_r($color_tran);
 
 ?>
 
-The above example will output something similar to:
+/* The above example will output something similar to:
 Array
 (
    [red] => 226
    [green] => 222
    [blue] => 252
    [alpha] => 0
-)
+) */
