4739c5dd96ac8188425099a2eaaf9ec04ba60c5b
thperkins <thperkins@users.noreply.github.com>
2013-10-17 10:36:43 +0200

    Updated
    Simpler method for extracting array from image

--- img-to-matrix.php
+++ img-to-matrix.php
@@ -1,5 +1,7 @@
 <?php
 
+/* COMMENTED OUT - SUPERSEDED BY BELOW
+
 // open an image
 $img = imagecreatefromjpeg('image.jpg'); // imagecreatefromjpeg/png/
 
@@ -14,13 +16,31 @@ $color_tran = imagecolorsforindex($img, $color_index);
 // what is it ?
 print_r($color_tran);
 
-?>
+*/
 
-/* The above example will output something similar to:
-Array
-(
-   [red] => 226
-   [green] => 222
-   [blue] => 252
-   [alpha] => 0
-) */
+// open an image
+$img = imagecreatefromjpeg('image.jpg'); // imagecreatefromjpeg/png/
+
+// get image dimension, define colour array
+$width = imagesx($image);
+$height = imagesy($image);
+$colors = array();
+
+
+for ($y = 0; $y < $height; $y++)
+{
+    for ($x = 0; $x < $width; $x++)
+    {
+        $rgb = imagecolorat($image, $x, $y);
+        $r = ($rgb >> 16) & 0xFF;
+        $g = ($rgb >> 8) & 0xFF;
+        $b = $rgb & 0xFF;
+
+//        $black = ($r == 0 && $g == 0 && $b == 0);
+//        $colors[$x][$y] = $black;
+    } 
+}
+
+print_r($colors)
+
+?>
